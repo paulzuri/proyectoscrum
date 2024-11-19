81f5148b9a6f97953d92805c7afa4867050837a5
Paul Zurita <paulzuri@hotmail.com>
2024-11-18 20:18:29 -0500

    Creado esqueleto del backend

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask Backend!"
+
+@app.route('/api', methods=['GET'])
+def api_example():
+    return jsonify({"message": "Hello, World!"})
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
