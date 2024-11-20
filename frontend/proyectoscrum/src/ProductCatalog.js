b026e1fc3f2826c5680eb9f916024c94b03169b7
DannaZal <danna.zaldumbide@epn.edu.ec>
2024-11-19 23:06:23 -0500

    Add ProductCatalog.js with simple container code

---
+++ frontend/proyectoscrum/src/ProductCatalog.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './ProductCatalog.css'; // Puedes crear este archivo para estilos
+
+const ProductCatalog = () => {
+ return (
+    <div className="product-catalog">
+        <h1>Catálogo de Productos</h1>
+        <div className="product-container">
+            {/* Aquí se mostrarán los productos */}
+        </div>
+    </div>
+    );
+};
+export default ProductCatalog;
