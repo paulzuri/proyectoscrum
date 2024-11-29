7c0d0294cc9f4c843b5aeda5fb3a7bc6c6de4d76
Paul Zurita <paulzuri@hotmail.com>
2024-11-29 00:42:10 -0500

    implementado llamado de tarjetas desde mongo

--- frontend/proyectoscrum/src/App.js
+++ frontend/proyectoscrum/src/App.js
@@ -8,41 +8,6 @@ import SearchBar from './SearchBar';  // Importa el componente SearchBar
 import Footer from './Footer'; // Importa el Footer
 const API_URL = "http://localhost:5555";
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-
-/*<ProductCatalog apiUrl={API_URL} />
-
-return (
-    <div className="App">      
-      <ProductCatalog />
-    </div>
-  );
-
-*/
-//Comprobar si funciona conexion
-
 function App() {
   const [message, setMessage] = useState(""); // Estado para almacenar la respuesta del backend
 
