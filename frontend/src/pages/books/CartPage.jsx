c2ed2361d7903b969c05e703ec11bd89964e78f8
WiloG2 <fexbius@gmail.com>
2025-01-15 01:19:56 -0500

    Cambios esteticos carrito de compras

--- frontend/src/pages/books/CartPage.jsx
+++ frontend/src/pages/books/CartPage.jsx
@@ -22,14 +22,14 @@ const CartPage = () => {
             <div className="flex mt-12 h-full flex-col overflow-hidden bg-white shadow-xl">
                 <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
                     <div className="flex items-start justify-between">
-                        <div className="text-lg font-medium text-gray-900">Shopping cart</div>
+                        <div className="text-lg font-medium text-gray-900">Carrito de compras</div>
                         <div className="ml-3 flex h-7 items-center ">
                             <button
                                 type="button"
                                 onClick={handleClearCart }
                                 className="relative -m-2 py-1 px-2 bg-red-500 text-white rounded-md hover:bg-secondary transition-all duration-200  "
                             >
-                                <span className="">Clear Cart</span>
+                                <span className="">Vaciar Carrito</span>
                             </button>
                         </div>
                     </div>
@@ -59,7 +59,7 @@ const CartPage = () => {
                                                                 </h3>
                                                                 <p className="sm:ml-4">${product?.newPrice}</p>
                                                             </div>
-                                                            <p className="mt-1 text-sm text-gray-500 capitalize"><strong>Category: </strong>{product?.category}</p>
+                                                            <p className="mt-1 text-sm text-gray-500 capitalize"><strong>Categoria: </strong>{product?.category}</p>
                                                         </div>
                                                         <div className="flex flex-1 flex-wrap items-end justify-between space-y-2 text-sm">
                                                             <p className="text-gray-500"><strong>Qty:</strong> 1</p>
@@ -68,7 +68,7 @@ const CartPage = () => {
                                                                 <button
                                                                 onClick={() => handleRemoveFromCart(product)}
                                                                 type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                                                    Remove
+                                                                    Eliminar
                                                                 </button>
                                                             </div>
                                                         </div>
@@ -80,7 +80,7 @@ const CartPage = () => {
 
 
                                     </ul>
-                                ) : (<p>No product found!</p>)
+                                ) : (<p>Ningun producto a sido encontrado!</p>)
                             }
 
 
@@ -93,24 +93,24 @@ const CartPage = () => {
                         <p>Subtotal</p>
                         <p>${totalPrice ? totalPrice : 0}</p>
                     </div>
-                    <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                    <p className="mt-0.5 text-sm text-gray-500">Sin IVA</p>
                     <div className="mt-6">
                         <Link
                             to="/checkout"
                             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
-                            Checkout
+                            Pagar
                         </Link>
                     </div>
                     <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <Link to="/">
-                            or
+                            o
                             <button
                                 type="button"
 
                                 className="font-medium text-indigo-600 hover:text-indigo-500 ml-1"
                             >
-                                Continue Shopping
+                                Continuar Comprando 
                                 <span aria-hidden="true"> &rarr;</span>
                             </button>
                         </Link>
