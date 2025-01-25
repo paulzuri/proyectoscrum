9143a284b56e48d145a5c8de4eae2759bd1f2414
Marlon Zurita <paulzuri@hotmail.com>
2025-01-24 20:30:34 -0500

    arreglado valor del checkout

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -11,7 +11,7 @@ import Paypal from './Paypal';
 
 const CheckoutPage = () => {
     const cartItems = useSelector(state => state.cart.cartItems);
-    const totalPrice = cartItems.reduce((acc, item) => acc + item.newPrice, 0).toFixed(2);
+    const totalPrice = cartItems.reduce((acc, item) => acc + item.newPrice * item.quantity, 0).toFixed(2);
     const { currentUser } = useAuth()
     const {
         register,
